cad492121ca4dc55310d86912dac619afc4604dc
Akbar Hidayatuloh <01akbarh@gmail.com>
2023-12-06 15:58:14 +0700

    init project

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam enim,
+      dolore accusantium harum deleniti voluptatem eos temporibus, laudantium
+      quasi beatae sint cum labore deserunt facilis, dolor eveniet est? Dolorem,
+      perferendis.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ProductDetail :product="product" />
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+const uri = "https://fakestoreapi.com/products/" + id;
+
+const { data: product } = await useFetch(uri);
+
+definePageMeta({
+  layout: "products",
+});
+</script>
+
+<style scoped></style>
